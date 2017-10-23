fe1f89cf3e106d1c182a345dc98cad8c621ab567
vszed <zyrpg@vip.qq.com>
2017-10-24 02:24:05 +0800

    attack step 02

--- Scripts/Attack.cs
+++ Scripts/Attack.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Attack : MonoBehaviour {
+public class Attack : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+			Debug.Log("attack~");
+			Character.getInstance().CharacStatus = Character.Status.attack;
+			Character.getInstance().shoot();
+        }
+    }
 }
